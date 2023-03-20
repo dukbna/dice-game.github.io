f614943a0ebb92bcc5f865e1a269b816c81a5214
duk <doke_the_lofer@yahoo.com>
2023-03-20 17:11:24 +0800

    shoog shiddeg bolgoh

--- app.js
+++ app.js
@@ -6,11 +6,17 @@ var scores = [0,0];
 // Тоглогчийн ээлжиндээ цуглуулж байгаа оноог хадгалах хувьсагч
 var roundScore = 0 ;
 //  Шооны аль талаараа буусныг хадгалах хувьсагч хэрэгтэй, 1-6 гэсэн утгыг энэ хувьсагчид санамсаргүйгээр үүсгэж өгнө.
-var dice = Math.floor (Math.random() * 6) + 1;
+var diceNumber = Math.floor (Math.random() * 6) + 1;
 
-window.document.querySelector("#score-0").textContent = 0;
-window.document.querySelector("#score-1").textContent = 0;
-window.document.querySelector("#current-0").textContent = 0;
-window.document.querySelector("#current-1").textContent = 0;
+window.document.getElementById("score-0").textContent = "0";
+window.document.getElementById("score-1").textContent = "0";
+window.document.getElementById("current-0").textContent = "0";
+window.document.getElementById("current-1").textContent = "0";
  
-document.querySelector(".dice").style.display = "none";
+var diceDom = document.querySelector(".dice")
+diceDom.style.display = "none";
+document.querySelector(".btn-roll").addEventListener("click",function(){
+    var diceNumber = Math.floor (Math.random() * 6) + 1;
+    diceDom.style.display = "block";
+    diceDom.src = "dice-" + diceNumber + ".png";
+});
